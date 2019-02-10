71a5ef4dcb9bc1bf3bc875387b9b9aaeba11bb4b
agtrevino <39744373+agtrevino@users.noreply.github.com>
2019-02-09 18:28:10 -0600

    updates

--- hodgepodgees2/src/app/app.module.ts
+++ hodgepodgees2/src/app/app.module.ts
@@ -7,6 +7,35 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+
+
+export const routerConfig: Routes = [
+  {
+      path: 'home',
+      component: HomePageComponent
+  },
+  {
+      path: 'about',
+      component: AboutPageComponent
+  },
+  {
+      path: 'products',
+      component: ProductsComponent
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -23,4 +52,5 @@ import { FooterComponent } from './footer/footer.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
